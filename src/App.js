fe92681daafa1c6b8dc1931f20c43a859b4070a5
Karthikeyan S <karthikeyanssvk@gmail.com>
2022-03-23 13:42:07 +0530

    modified for gh-pages deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import RouteList from "./screens/RouteList";
 
 function App() {
